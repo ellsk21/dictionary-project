59d6a52770680c7c22f6973778c0379170d0c74a
Ellie Korth <87391629+ellsk21@users.noreply.github.com>
2023-08-25 10:53:15 +1200

    added new component
    dded a form and searching abilities. also added dictionary component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-
+import Dictionary from "./Dictionary.js"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+       <div className="container">
       <header className="App=header">
-      <h1>Hello from New Zealand</h1>
+       
+      <main>
+        <Dictionary />
+      </main>
       </header>
-      
+      </div>
+      <footer>
+        Coded by Ellie Korth 
+      </footer>
     </div>
   );
 }
