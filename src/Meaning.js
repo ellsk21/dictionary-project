92bba83dc42765b4a41d03effc0df2c79a3dbd38
Ellie Korth <87391629+ellsk21@users.noreply.github.com>
2023-08-26 13:58:43 +1200

    added components
    added results and meaning

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,14 +1,19 @@
 import React from "react";
 
+
 export default function Meaning(props) {
     console.log(props.meaning);
     return (
         <div className="Meaning">
-            <h3>{props.meaning.partOfSpeech}</h3>
-            <p>
-        <strong>Definition</strong> {props.meaning.definition}
-        <br />
-      </p>
-        </div>
-    )
+    <h3>{props.meaning.partOfSpeech}</h3>
+    
+    <p>
+        <strong>Definition:</strong> {props.meaning.definition}</p>
+    
+    <p>
+    <strong>Example:</strong> {props.meaning.example}
+    </p>
+    </div>
+    );
+    
 }
